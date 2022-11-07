a84f4df2bbb8c99f27a1d56e02984d3114b24003
DhioAWP <dhioanugerah0@gmail.com>
2022-11-07 16:57:53 +0700

    add class

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -2,7 +2,18 @@
 
 namespace single_linked_list
 {
+    //each node consist of the information part and link to the next node
     class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+    }
+    class list
+    {
+     
+    }
     class program
     {
         static void Main(string[] args)
